fad1ccf871fd3a4c025d024d7a61cf4d3318dc4d
hiba764 <hibaajam03@gmail.com>
2026-04-28 23:41:18 +0300

    Add UserService

--- Moeen.Api/Shared/Requests/Identity/VerifyEmailRequest.cs
+++ Moeen.Api/Shared/Requests/Identity/VerifyEmailRequest.cs
@@ -1,6 +1,10 @@
 ﻿namespace Moeen.Api.Shared.Requests.Identity
 {
-    public class VerifyEmailRequest
-    {
+  
+        public class VerifyEmailRequest
+        {
+            public string? Code { get; set; }
+            public string? Email { get; set; }
+            public Guid? UserId { get; set; }
+        }
     }
-}
