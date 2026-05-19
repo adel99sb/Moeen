7be13d85a6b278569c1c57f20ceaa6ab67aa2cbf
Alaa <Duaa@DESKTOP-QVMEGPN>
2026-05-20 00:04:15 +0300

    The PointService and MosquService done

---
+++ Moeen.Dashboard/Services/Abstractions/IMosquService.cs
@@ -0,0 +1,20 @@
+﻿using Moeen.Shared.Requests.Mosuq;
+using Moeen.Shared.Responses;
+using System;
+using System.Threading.Tasks;
+namespace Moeen.Dashboard.Services.Abstractions
+{
+    public interface IMosquService
+    {
+        Task<GeneralResponse> AddMosqu(AddMosquReq req);
+        Task<GeneralResponse> GetAllMosqus(GetAllMosqusRequest request);
+        Task<GeneralResponse> GetMosqueByIdAsync(Guid mosqueId);
+        Task<GeneralResponse> GetCirclesByMosqueAsync(GetCirclesByMosqueRequest request);
+        Task<GeneralResponse> GetTeachersByMosqueAsync(GetTeachersByMosqueRequest request);
+        Task<GeneralResponse> GetMosqueStatisticsAsync(GetMosqueStatisticsRequest request);
+        Task<GeneralResponse> UpdateMosqueInfoAsync(UpdateMosqueInfoRequest request);
+        Task<GeneralResponse> AssignMosqueAdminAsync(AssignMosqueAdminRequest request);
+        Task<GeneralResponse> DeleteMosqueAsync(DeleteMosqueRequest request);
+        Task<GeneralResponse> UnassignMosqueAdminAsync(UnassignMosqueAdminRequest request);
+    }
+}
