6052abe0710d4d57b5bb1aee514396f2af1aaaff
ItsAhmadoof <Ahmad.H@variin.com>
2026-07-13 17:38:46 +0300

    Use HTTP API base URL in app

--- Moeen.App/Infrastructure/Http/ApiRouts.cs
+++ Moeen.App/Infrastructure/Http/ApiRouts.cs
@@ -3,7 +3,7 @@
     public static class ApiRoutes
     {
 
-        public static string BaseUrl { get; } = "https://moeen.somee.com/";
+        public static string BaseUrl { get; } = "http://moeen.somee.com/";
 
         // Auth
         public static string LoginRoute { get; } = "api/User/login";
@@ -23,7 +23,7 @@
         public static string ParentLibraryBooksRoute { get; } = "api/mobile/parent-library/books";
         public static string ParentPostsMeRoute { get; } = "api/mobile/parent-posts/me";
         public static string ParentPostToggleLikeRoute(Guid postId) => $"api/mobile/parent-posts/{postId}/toggle-like";
-        
+
         // Content Sharing
         public static string GetAllContentSharingRoute { get; }
             = "api/ContentSharing/all";
